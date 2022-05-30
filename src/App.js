74ad4e5ac07a92bfbc3fc4f26a87ceea6f0a28be
matcha <massahiro.endo@gmail.com>
2022-05-31 00:23:55 +0200

    work before input in amount input box

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ const LATEST_CURRENCY_URL = `${BASE_URL}/latest`;
 
 function App() {
   // const [currency, setCurrency] = useState("EUR");
+  const [exchangeDate, setExchangeDate] = useState("");
   const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [amount, setAmount] = useState(1);
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amountIsFromCurrency, setAmountIsFromCurrency] = useState(true);
 
   // const makeLiveUrl = (curr) => `${LATEST_CURRENCY_URL}?$base=${curr}`;
 
@@ -26,16 +32,36 @@ function App() {
     })
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
+        setExchangeDate(data.date);
+        const firstCurrency = Object.keys(data.rates)[149];
         setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency]);
+        //   const XAU = Object.keys(data.rates)[158];
+        //   const XAG = Object.keys(data.rates)[157];
+        //   setCurrencyOptions([XAU, ...Object.keys(data.rates)]);
+        //   setFromCurrency(XAU);
+        //   setToCurrency(XAG);
+        //   setExchangeRate(data.rates[XAU] / data.rates[XAG]);
       });
   }, []);
 
   return (
     <>
-      <h1>Converter</h1>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <h1>Today is {exchangeDate} and exchange rate is:</h1>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(event) => setFromCurrency(event.target.value)}
+      />
       <div className="equals">=</div>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(event) => setToCurrency(event.target.value)}
+      />
     </>
   );
 }
--- src/component/CurrencyRow.js
+++ src/component/CurrencyRow.js
@@ -2,11 +2,11 @@ import React from "react";
 import classes from "./CurrencyRow.module.css";
 
 const CurrencyRow = (props) => {
-  const { currencyOptions } = props;
+  const { currencyOptions, selectedCurrency, onChangeCurrency } = props;
   return (
     <div>
       <input type="number" className={classes.input} />
-      <select>
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map((option, index) => (
           <option key={index} value={option}>
             {option}
